ba3c38c17b68f5843791ff0448c4daa5388a8bcf
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-20 21:16:01 +0600

    Successfully data sent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(json => setUser(json))
-  }, [])
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((json) => setUser(json));
+  }, []);
+  const addUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const user = { name };
+    console.log(user);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      });
+  }, [user]);
+
   return (
     <div className="App">
       <h1>Total User: {user.length}</h1>
+      <form onSubmit={addUser}>
+        <input type="text" name="name" id="" />
+        <input type="submit" value="Add User" />
+      </form>
       <ul>
-        {
-          user.map(person => <li key={person.id}>{person.name}</li>)
-        }
+        {user.map((person) => (
+          <li key={person.id}>
+            {" "}
+            {person.id} ~ {person.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
